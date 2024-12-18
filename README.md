16f152189c3d20980b0c84930acb4544d1b7552d
treska03 <bartlomiej.treska@gmail.com>
2024-12-18 18:28:56 +0100

    Init commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Intersection simulation
+
+--------
+A simple intersection simulation for recrutation at AVSystem
+
+
+## How to run:
+1. Clone the project
+2. Prepare a file containing information about intersection (vehicles etc.)
+3. Run `IntersectionSimulationApplication.kt` with path to your file as console arg.
---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+	kotlin("jvm") version "1.9.25"
+	kotlin("plugin.spring") version "1.9.25"
+	id("org.springframework.boot") version "3.4.0"
+	id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "pl.treska"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.2")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testImplementation("io.mockk:mockk:1.13.13")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict")
+	}
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+	kotlinOptions.jvmTarget = "21"
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
